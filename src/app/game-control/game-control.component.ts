cf079d0a35acd5a8a8eda9e09fa63fb32451ce6d
Jeremy Villa <villajeremy1991@gmail.com>
2023-10-02 18:01:53 -0400

    Event emitter set up to emit the number every 1 second

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  @Output() intervalEvent = new EventEmitter<{ interval: number }>;
+  intervalId?: NodeJS.Timeout;
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+  onStart() {
+    let incrementer = 0;
+    console.log("game Started");
+    this.intervalId = setInterval(
+      () => {
+        this.intervalEvent.emit({ interval: incrementer });
+        incrementer++;
+        console.log(incrementer);
+      }
+      , 1000);
+  }
+
+  onStop() {
+    clearInterval(this.intervalId);
+  }
+}
