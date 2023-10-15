b24af812c0e9c9eb9be4794165aa56666174bc02
Jeremy Villa <villajeremy1991@gmail.com>
2023-10-15 14:35:25 -0400

    Added property binding to the BetterHighlightDirective

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  // 4. We can also bind the colors to properties dynamically. For example,
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
   // 3. We can totally take our renderer if we use Host Binding to bind to the HostProperty
   @HostBinding('style.backgroundColor') backgroundColor : string = 'transparent';
 
@@ -12,7 +15,10 @@ export class BetterHighlightDirective implements OnInit {
 
   ngOnInit(): void {
     // 1. This is fine, but we use HostListener to listen to events that will allow use to dynamically change the backgrounds
-    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+
+    // 4. This value will be given from the html side
+    this.backgroundColor = this.defaultColor;
   }
 
   // 2. HostListener will listen to events from the DOM
@@ -20,7 +26,9 @@ export class BetterHighlightDirective implements OnInit {
     //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
 
     // 3. We comment out the line above with the renderer as we're accessing the HTML property using the Host Binding
-    this.backgroundColor = 'blue';
+
+    // 4. This value will be given from the html side
+    this.backgroundColor = this.highlightColor;
   }
 
   // 2. HostListener will listen to events from the DOM
